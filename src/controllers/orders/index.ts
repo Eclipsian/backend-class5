3b0860224c0d55a04b638067557a195289b305d5
Eclipsian <erica2007210@gmail.com>
2022-09-27 23:10:51 -0400

    hw8

--- src/controllers/orders/index.ts
+++ src/controllers/orders/index.ts
@@ -2,51 +2,65 @@ import { prisma } from '../..';
 import { Request, Response } from 'express';
 
 export const getAllOrders = async (req: Request, res: Response) => {
-	const orders = await prisma.order.findMany({
-		include: {
-			products: true,
-			student: true,
-		},
-	});
-	res.json(orders);
+  const orders = await prisma.order.findMany({
+    include: {
+      products: true,
+      student: true,
+    },
+  });
+  res.json(orders);
 };
 
 export const createOrder = async (req: Request, res: Response) => {
-	const { studentId, products } = req.body;
-	const order = await prisma.order.create({
-		data: {
-			student: {
-				connect: {
-					id: Number(studentId),
-				},
-			},
-			products: {
-				connect: (products as []).map((productId: string) => ({
-					id: Number(productId),
-				})),
-			},
-		},
-		include: {
-			products: true,
-			student : true,
-		},
-	});
-	res.json(order);
+  const { studentId, products } = req.body;
+  const order = await prisma.order.create({
+    data: {
+      student: {
+        connect: {
+          id: Number(studentId),
+        },
+      },
+      products: {
+        connect: (products as []).map((productId: string) => ({
+          id: Number(productId),
+        })),
+      },
+    },
+    include: {
+      products: true,
+	  student : true,
+	},
+  });
+  res.json(order);
 };
 
+
+
 export const deleteOrder = async (req: Request, res: Response) => {
-	const { id } = req.body;
-	try { 
-	  const order = await prisma.order.delete({
-		where: {
-		  id: Number(id),
-		},
-	  });
-	  res.json(order);
-	} catch (error) {
-	  console.log(error);
-	  res.json({
-		message: 'Something went wrong'
+  const { id } = req.body;
+  try { 
+	const existingOrder = await prisma.order.findFirst({
+	  where: {
+		id: Number(id),
+	  },
+	});
+	if (!existingOrder) {
+  	  return res.json({
+		message: 'Order does not exist',
 	  });
 	}
-  };
+
+	const order = await prisma.order.delete({
+	  where: {
+	    id: Number(id),
+	  },
+	});
+	res.json(order);
+  } catch (error) {
+	console.log(error);
+	res.json({
+	  message: 'Something went wrong'
+	});
+  }
+};
+

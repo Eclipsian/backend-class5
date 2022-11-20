446ccb75031bb35e8c38c7f537ff744304438001
Eclipsian <erica2007210@gmail.com>
2022-11-20 18:16:38 -0500

    cw-15

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createCategory, deleteCategory, getAllCategories, updateCategory, } from '../controllers/category';
+import { createCategory, deleteCategory, getAllCategories, getCategoryById, updateCategory, } from '../controllers/category';
 
 const categoryRouter = Router();
 
 categoryRouter.get('/', getAllCategories);
+categoryRouter.get('/:id', getCategoryById)
 categoryRouter.post('/', createCategory);
 categoryRouter.delete('/', deleteCategory);
 categoryRouter.put('/', updateCategory);

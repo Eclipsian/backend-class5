9a0daeac53803802e567be0b39ae65005f0f5569
Eclipsian <erica2007210@gmail.com>
2023-03-05 23:21:08 -0500

    cw-24 + 25

--- src/controllers/subcategory/index.ts
+++ src/controllers/subcategory/index.ts
@@ -1,49 +1,71 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import { prisma } from '../..';
 
 export const getAllSubcategory = async (req: Request, res: Response) => {
-  const data = await prisma.subCategory.findMany();
-  res.json(data);
+	const data = await prisma.subCategory.findMany();
+	res.json(data);
 };
 
 
-export const getAllProductsBySubcategory = async (req: Request, res: Response) => {
-  const { slug } = req.params;
-  const { search, priceFilters } = req.query;
-  const subCategory = await prisma.subCategory.findFirst({
+export const getAllProductsBySubcategory = async (
+	req: Request,
+	res: Response,
+) => {
+	const { slug } = req.params;
+	const { search, priceFilters, sorting } = req.query;
+	const subCategory = await prisma.subCategory.findFirst({
 		where: {
 			slug,
 		},
 	});
 
-  const priceFilterArr = priceFilters as {min: string, max: string}[];
+	const priceFilterArr = priceFilters as { min: string; max: string }[];
 
-  const products = await prisma.product.findMany({
-    where: {
-      AND: [
-        {
-          title: {
-            contains: search as string,
-            mode: 'insensitive',
-          }
-        },
-        {
-          subCategoryId: subCategory?.id,
-        },
-        {
-          OR: priceFilters && priceFilterArr.length > 0 ? priceFilterArr.map((price: { min: string, max: string }) => ({
-            price: {
-              lte: Number(price.max),
-              gte: Number(price.min),
-            }
-          })) : undefined,
-        }
-      ],
-    }
-  });
+	const sortingArr = sorting as { field: string; order: string }[];
+
+	const productSortingLogic =
+		sortingArr && sortingArr.length
+			? sortingArr.map(({ field, order }) => {
+					return {
+						[field]: order,
+					};
+			  })
+			: [
+					{
+						price: 'asc',
+					},
+			  ];
+
+	const products = await prisma.product.findMany({
+		where: {
+			AND: [
+				{
+					title: {
+						contains: search as string,
+						mode: 'insensitive',
+					},
+				},
+				{
+					subCategoryId: subCategory?.id,
+				},
+				{
+					OR:
+						priceFilters && priceFilterArr.length > 0
+							? priceFilterArr.map((price: { min: string; max: string }) => ({
+									price: {
+										lte: Number(price.max),
+										gte: Number(price.min),
+									},
+							  }))
+							: undefined,
+				},
+			],
+		},
+		orderBy: [...productSortingLogic],
+	});
 
 	res.json({
-    ...subCategory,
-    products
-  });
+		...subCategory,
+		products,
+	});
 };
